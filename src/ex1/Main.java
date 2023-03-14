ac01ff22c5cb274c9b82c7b8811f7bbae71dd1a1
Miriam Binyamines <89273650+MiriamBin@users.noreply.github.com>
2023-03-14 20:56:20 +0200

    changed the main

--- src/ex1/Main.java
+++ src/ex1/Main.java
@@ -7,18 +7,21 @@ import java.util.Scanner;
  */
 public class Main {
     /**
-     * * The main method of the program.
+     * The main method of the program.
      * @param args not used
      */
     public static void main(String[] args) {
 
-        Counter counter = new Counter();
-        int numOfNums = readNumber("Please enter the number of numbers you would like to enter");
+        Counter counter = new Counter();            // Counter object
+        Scanner scanner = new Scanner(System.in);   // Scanner for input from user
 
-        System.out.println("Please enter " + numOfNums + " values");
+        System.out.print("Please enter the number of numbers you would like to enter: ");
+        int numOfNums = readNumber(scanner);
 
+        System.out.printf("Please enter %d positive integer values: ", numOfNums);
+        // Reading values from the user and summing them
         for (int i = 0; i < numOfNums; i++) {
-            int num = readNumber("Please enter a positive integer");
+            int num = readNumber(scanner);
             counter.increment(num);
         }
         counter.print();
@@ -26,20 +29,18 @@ public class Main {
 
     /**
      * Reads user input using a scanner and ensures that only positive integers are accepted.
-     * @param msg the message to prompt the user for input
+     * @param scanner for reading input from the user and check is validation
      * @return The positive integer entered by the user.
      */
-   public static int readNumber(String msg) {
+    public static int readNumber(Scanner scanner) {
 
-        Scanner scanner = new Scanner(System.in);
         int num;
         do {
-            System.out.print(msg + ": ");
-
             while (!scanner.hasNextInt()) {
-                System.err.println("Error: Input is not a number.");
+                System.err.println("Error: Input is not an integer.");
                 scanner.next();
             }
+
             num = scanner.nextInt();
 
             if (num <= 0) {
--- src/ex1/MainMax.java
+++ src/ex1/MainMax.java
@@ -9,20 +9,25 @@ import java.util.Scanner;
  */
 public class MainMax {
     /**
-     * * The main method of the program.
+     * The mainMax method of the program.
      * @param args not used
      */
     public static void main(String[] args) {
 
-        int max = readNumber("Please enter the max value");
-        int numOfNums = readNumber("Please enter the number of numbers you would like to enter");
-        CounterMax counter = new CounterMax(max);
+        Scanner scanner = new Scanner(System.in);            // Scanner for input from user
 
-        System.out.println("Please enter " + numOfNums + " values");
+        System.out.print("Please enter the max value: ");
+        int max = readNumber(scanner);
+        CounterMax counter = new CounterMax(max);           // Counter object
 
+        System.out.print("Please enter the number of numbers you would like to enter: ");
+        int numOfNums = readNumber(scanner);
+
+        System.out.printf("Please enter %d positive integer values: ", numOfNums);
         // Reading values from the user and summing them
+        int num;
         for (int i = 0; i < numOfNums; i++) {
-            int num = readNumber("Please enter a positive integer");
+            num = readNumber(scanner);
             try {
                 counter.increment(num);
             }
@@ -36,19 +41,18 @@ public class MainMax {
 
     /**
      * Reads user input using a scanner and ensures that only positive integers are accepted.
-     * @param msg the message to prompt the user for input
+     * @param scanner for reading input from the user and check is validation
      * @return The positive integer entered by the user.
      */
-    public static int readNumber(String msg) {
-        Scanner scanner = new Scanner(System.in);
+    public static int readNumber(Scanner scanner) {
+
         int num;
         do {
-            System.out.print(msg + ": ");
-
             while (!scanner.hasNextInt()) {
-                System.err.println("Error: Input is not a number.");
+                System.err.println("Error: Input is not an integer.");
                 scanner.next();
             }
+
             num = scanner.nextInt();
 
             if (num <= 0) {
